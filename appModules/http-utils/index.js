1cfcba29b92d5b23e220b1c6d9c2b0446f5bfc34
SSerov77 <sergo.sergej.05@yandex.ru>
2024-05-12 22:27:01 +0500

    дэзэшечка игровой лаунчер

---
+++ appModules/http-utils/index.js
@@ -0,0 +1,9 @@
+const staticFile = require("./static-file");
+const parseBody = require("./parse-body");
+const mimeTypes = require("./mime-types");
+
+module.exports = {
+	staticFile,
+	parseBody,
+	mimeTypes,
+};
